68598b6b1e172924de4925c37c112fd9141861ed
sankalp patel <sankalppatel38@gmail.com>
2026-06-20 13:41:17 +0530

    variation_nonRepeatElement

--- Problem1.js
+++ Problem1.js
@@ -1,4 +1,11 @@
-// Find First Non-repeating character in string (Interview Question)
+//#region Non Repeating Character
+
+// Problem Statement: Given a string str consisting of lowercase English Letters. return the first non-repeating character in str. If there is no non-repeating character, return '$'.
+
+// Inputs:
+// Input: s = "aabbcdeff" Output: 'c'
+// Input: s = "racecar" Output: 'e'
+// Input: s = "aabbccc" Output: '$'
 
 // Solution
 // function NonRepeating(str) {
@@ -17,34 +24,63 @@
 //     }
 //   }
 
-//   return null
+//   return '$'
 // }
+// let result = NonRepeating('aabbcdeff')
+// console.log(result)
 
-let result = NonRepeating('aabbcdeff')
-
-console.log(result)
-
-// Variation (if they asked index of the first character or element)
+// Trap : (if they asked index of the first character or element)
 //solution:  simply we run a replace (for..of) with (for loop) in second pass and return i;
 
-function NonRepeating(str) {
-  if (!str) return
+// function NonRepeating(str) {
+//   if (!str) return
 
-  // first pass to store the frequency of Characters
-  let freq = {}
-  for (const key of str) {
+//   // first pass to store the frequency of Characters
+//   let freq = {}
+//   for (const key of str) {
+//     freq[key] = (freq[key] || 0) + 1
+//   }
+
+//   // second pass to check for first non-repeating character
+//   for(let i = 0 ; i<= str.length-1; i++){
+//     if(freq[str[i]] === 1){
+//       return i
+//     }
+//   }
+
+//   return -1
+// }
+
+// variation
+
+//#region Non-Repeating Element
+
+// Problem statement: Find the first non-repeating element in a given array arr of integers and if there is not present any non-repeating element then return 0
+
+// Inputs
+// Input: arr[] = [-1, 2, -1, 3, 2] Output: 3
+// Input: arr[] = [1, 1, 1] Output: 0
+
+// solution
+function NonRepeatingElement(arr) {
+  const freq = {}
+  if (!arr) return
+
+  for (const key of arr) {
     freq[key] = (freq[key] || 0) + 1
   }
 
-  // second pass to check for first non-repeating character
-  for(let i = 0 ; i<= str.length-1; i++){
-    if(freq[str[i]] === 1){
-      return i
+  for (const key of arr) {
+    if (freq[key] === 1) {
+      return key
     }
   }
 
-  return -1
+  return 0
 }
+
+const ResultElement = NonRepeatingElement([-1, -1, -1])
+console.log(ResultElement)
 
 // Note: last m kya return krna hai wo question ke hisab se rahenge, return according to that.
 // It means:
